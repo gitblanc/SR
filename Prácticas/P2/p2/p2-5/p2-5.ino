86ac35bb02dee7b81d9603ff86a6f1a3eb29a30f
Eduardo Blanco Bielsa <blanc@MacBook-Pro-de-Eduardo.local>
2022-10-03 18:05:38 +0200

    p2-5

--- Prácticas/P2/p2/p2-5/p2-5.ino
+++ Prácticas/P2/p2/p2-5/p2-5.ino
@@ -1,4 +1,5 @@
 #include <Keypad.h>
+#include <TM1637.h>
 //Leds
 int ledRojo = 2;
 int ledVerde = 3;
@@ -35,8 +36,16 @@ long responseTime;
 int pinTrig = 9;
 int pinEcho = 8;
 
-// Sensor PIR
-int sensorPir = 10;
+// Sensor de luz
+int sensorLuz = A4;
+
+//Pantalla de segmentos
+int pinClk = 10;
+int pinDio = 11;
+TM1637 screen(pinClk, pinDio);
+
+int cont = 0;
+bool entroUltrasonidos = false;
 
 void setup() {
   Serial.begin(9600);
@@ -46,8 +55,12 @@ void setup() {
   pinMode(pinTrig, OUTPUT);
   pinMode(pinEcho, INPUT);
   digitalWrite(ledVerde, HIGH);
-  //Sensor PIR
-  pinMode(sensorPir, INPUT);
+  //Sensor de luz
+  pinMode(sensorLuz, INPUT);
+  //Pantalla de segmentos
+  screen.init();
+  screen.set(BRIGHT_TYPICAL);
+  delay(1500);//esperamos a que se inicie la pantalla
 }
 
 void loop() {
@@ -59,6 +72,20 @@ void loop() {
       digitalWrite(ledVerde, LOW);
       digitalWrite(ledRojo, HIGH);
       sizeOfBuffer = 0;
+  }else if(teclaPulsada == 'C'&& isOpened){
+        isOpened = false;
+        Serial.println("Tecla pulsada: C -> puerta CERRADA");
+        digitalWrite(ledRojo, LOW);
+        digitalWrite(ledVerde, HIGH);
+        bufferLectura = ""; // reinicio del buffer
+        sizeOfBuffer = 0;
+  }
+  else if (hayPersonasInterior() < 100 && hayPersonasExterior() < 10 && isOpened){
+    inicioCuentaTiempo = millis();
+    isOpened = true;
+    Serial.println("Objeto grande detectado -> puerta ABIERTA");
+    digitalWrite(ledRojo, HIGH);
+    digitalWrite(ledVerde, LOW);
   }
   else if(millis() - inicioCuentaTiempo >= 5000 && isOpened){
         isOpened = false;
@@ -67,21 +94,21 @@ void loop() {
         bufferLectura = ""; // reinicio del buffer
         sizeOfBuffer = 0;
         Serial.println("Pasaron 5 segundos -> puerta CERRADA");
-  }else if(teclaPulsada == 'C'&& isOpened){
-        isOpened = false;
-        Serial.println("Tecla pulsada: C -> puerta CERRADA");
-        digitalWrite(ledRojo, LOW);
-        digitalWrite(ledVerde, HIGH);
-        bufferLectura = ""; // reinicio del buffer
-        sizeOfBuffer = 0;
-  }else if(hayPersonasInterior() == HIGH && isOpened){
+  }else if(hayPersonasInterior() < 100 && isOpened){//Caso 3 sensor de luz
         Serial.println("Hay personas en el interior -> PUERTA CERRADA");
         digitalWrite(ledRojo, LOW);
         digitalWrite(ledVerde, HIGH);
         bufferLectura = ""; // reinicio del buffer
         sizeOfBuffer = 0;
+        if(entroUltrasonidos){
+          cont+=1;
+          mostrarPantalla(cont);
+          entroUltrasonidos = false;
+        }
+        isOpened = false;
   }else if(hayPersonasExterior() < 10 && isOpened){
-    Serial.println("Hay personas a " + String(distance) + " cm -> puerta ABIERTA");
+    Serial.println("Hay personas en el interior -> puerta ABIERTA");
+    entroUltrasonidos = true;
     isOpened = false;
     sizeOfBuffer = 0;
   }else if(sizeOfBuffer > 3 && !isOpened){
@@ -116,5 +143,22 @@ long hayPersonasExterior(){
 }
 
 int hayPersonasInterior(){
-  return digitalRead(sensorPir);
+  return analogRead(sensorLuz);
+}
+
+void mostrarPantalla(int contador){
+distance = contador;
+//4.-
+int digit0 = distance/1000; 
+Serial.println("Digit0 "+ String(digit0));
+int digit1 = (distance - digit0*1000)/100; 
+Serial.println("Digit1 "+ String(digit1));
+int digit2 = (distance - (digit0*1000 + digit1*100))/10; 
+Serial.println("Digit2 "+ String(digit2));
+int digit3 = distance - (digit0*1000 + digit1*100 + digit2*10);
+Serial.println("Digit3 "+ String(digit3));
+screen.display(0, digit0); 
+screen.display(1, digit1); 
+screen.display(2, digit2); 
+screen.display(3, digit3);
 }

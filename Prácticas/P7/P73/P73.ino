3e733fb15fb39ca956756ae19efd0358a1b52f1f
Eduardo Blanco Bielsa <blanc@MacBook-Pro-de-Eduardo.local>
2022-11-07 17:57:36 +0100

    p7

--- Prácticas/P7/P73/P73.ino
+++ Prácticas/P7/P73/P73.ino
@@ -15,52 +15,96 @@ int LINE = HIGH; // También podría ponerse 1
 int pinIrDer = 3; 
 int pinIrIzq = 2;
 
-int contEspiral = 91;
+//int contEspiral = 91;
+int contEspiral = 89;
+
+//ultrasonidos
+int pinTrig = 4;
+int pinEcho = 5;
+
+bool estaEsquivando = false;
 
 void setup(){ 
   Serial.begin(9600); // Descomentar si queréis debuguear por consola 
   pinMode(pinIrDer, INPUT);
   pinMode(pinIrIzq, INPUT);
+  pinMode(pinTrig, OUTPUT);
+  pinMode(pinEcho, INPUT);
   servoLeft.attach(pinServoLeft); 
   servoRight.attach(pinServoRight);
+  ultrasonidos();
 }
 void loop() {
   detectarLinea();
 }
 
+bool detectarObstaculo(){
+  long distance = ultrasonidos();
+  return distance <= 20;
+ }
+
 void detectarLinea(){
   if(digitalRead(pinIrIzq) == LINE && digitalRead(pinIrDer) == LINE){
-    goForward();
-    contEspiral = 91;
+    if(detectarObstaculo()){
+      rodearObstaculo();
+    }else{
+      goForward();
+    }
+    //contEspiral = 91;
+    contEspiral = 89;
   }else if(digitalRead(pinIrIzq) == LINE && digitalRead(pinIrDer) == NO_LINE){
     left();
-    contEspiral = 91;
+    //contEspiral = 91;
+    contEspiral = 89;
   }else if(digitalRead(pinIrIzq) == NO_LINE && digitalRead(pinIrDer) == LINE){
     right();
-    contEspiral = 91;
+    //contEspiral = 91;
+    contEspiral = 89;
   }else if(digitalRead(pinIrIzq) == NO_LINE && digitalRead(pinIrDer) == NO_LINE){
-    espiral();
-    sumarContador();
+    if(detectarObstaculo()){
+      rodearObstaculo();
+    }else if(estaEsquivando){
+      estaEsquivando = false;
+      recuperarLinea();
+    }else{
+      espiral();
+      sumarContador();
+    }
   }
 }
 
+void recuperarLinea(){
+  goForward();
+  delay(2000);
+  right();
+  delay(1200);
+  goForward();
+  delay(3000);
+  right();
+  delay(1200);
+}
+
 void sumarContador(){
-  contEspiral++;
+  //contEspiral++;
+  contEspiral--;
   delay(1000);
 }
 
 void goForward(){
   // Adelante
-  servoLeft.write(180); // Velocidad Máxima 
-  servoRight.write(0);
+  //servoLeft.write(180); // Velocidad Máxima 
+  //servoRight.write(0);
+  servoLeft.write(0);
+  servoRight.write(180);
   
 }
 
 void goBackward(){
   // Atrás
-  servoLeft.write(0);
-  servoRight.write(180);
-  
+  //servoLeft.write(0);
+  //servoRight.write(180);
+  servoLeft.write(180);
+  servoRight.write(0);
 }
 
 void stopMovement(){
@@ -71,18 +115,45 @@ void stopMovement(){
 }
 
 void left(){
+   //servoLeft.write(90); 
+   //servoRight.write(0); 
    servoLeft.write(90); 
-   servoRight.write(0); 
-   
+   servoRight.write(180); 
 }
 
 void right(){
-   servoLeft.write(180); 
-   servoRight.write(90); 
-    
+   //servoLeft.write(180); 
+   //servoRight.write(90); 
+   servoLeft.write(0); 
+   servoRight.write(90);  
 }
 
 void espiral(){
+   //servoLeft.write(contEspiral); 
+   //servoRight.write(0);
    servoLeft.write(contEspiral); 
-   servoRight.write(0);
+   servoRight.write(180);
+}
+
+void rodearObstaculo(){
+  estaEsquivando = true;
+  left();
+  delay(1500);
+  goForward();
+  delay(2000);
+  right();
+  delay(1000);
+  detectarLinea();
+}
+
+long ultrasonidos(){
+  digitalWrite(pinTrig, LOW);
+  delayMicroseconds(10);
+  digitalWrite(pinTrig, HIGH);//enviamos el pulso
+  delayMicroseconds(10);
+  long responseTime = pulseIn(pinEcho, HIGH);// Medimos la longitud
+  //Serial.println("Tiempo: " + String(responseTime) + " microsegundos");
+  long distance = 0.01716*responseTime;// Calculamos la distancia una vez conocemos la velocidad
+  delay(100);
+  return distance;
 }

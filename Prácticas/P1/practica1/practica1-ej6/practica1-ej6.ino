8896505d959a34debfbbe9bedb6e517272fe2656
Eduardo Blanco Bielsa <blanc@MacBook-Pro-de-Eduardo.local>
2022-11-28 23:54:59 +0100

    p1.6

--- Prácticas/P1/practica1/practica1-ej6/practica1-ej6.ino
+++ Prácticas/P1/practica1/practica1-ej6/practica1-ej6.ino
@@ -1,19 +1,17 @@
 //LEDS
 //led 1 izq
-int green1 = 10;
-int blue1 = 9;
-int red1 = 8;
+int green1 = 12;
+int blue1 = 11;
+int red1 = 10;
 //led 2 der
-int green2 = 6;
-int blue2 = 5;
+int green2 = 9;
+int blue2 = 8;
 int red2 = 7;
-//peatones
-int greenP = 6;
-int redP = 5;
 
 //BUZZER
-int pinbuzzer = 12;
-int valorBuzzer = 600;
+int pinbuzzer = 6;
+int minBuzzer = 0;
+int maxBuzzer = 600;
 
 //POTENCIOMETROS
 int p1 = A2;
@@ -31,52 +29,39 @@ void setup(){
 }
 
 void loop(){
+  int frecuencia = map(analogRead(p2), 0, 1023, minBuzzer, maxBuzzer);
   int pot1Value = analogRead(p1);
-  Serial.println(pot1Value);
-  if(pot1Value<=20){
+  hacerSonarBuzzer(frecuencia);
+  if(pot1Value<=40){//apagado,blanco
        asignarLed1(0,0,0);
        asignarLed2(255,255,255);
-  }else if(pot1Value <= 255){
+  }else if(pot1Value <= 255){//rojo,azul
+    asignarLed1(pot1Value,0,0);
+    asignarLed2(0,pot1Value,0);
+  }else if(pot1Value <= 400){//morado, rosa
+    asignarLed1(255,255,0);
+    asignarLed2(255,20,0);
+  }else if(pot1Value <= 510){//verde,verde
     asignarLed1(0,0,pot1Value);
-  }else if(pot1Value <= 510){
-    asignarLed1(0,pot1Value-510,0);
+    asignarLed2(0,0,pot1Value);
+  }else if(pot1Value <= 610){//verde,amarillo
+    asignarLed1(0,0,pot1Value);
+    asignarLed2(pot1Value,0,pot1Value);
+  }else if(pot1Value <= 765){//amarillo,verde
+    asignarLed1(pot1Value,0,pot1Value);
+    asignarLed2(0,0,pot1Value);
+  }else if(pot1Value <= 850){//morado,rosa
+    asignarLed1(pot1Value,pot1Value,0);
+    asignarLed2(pot1Value,20,0);
+  }else if(pot1Value <= 950){//azul,rojo
+    asignarLed1(0,pot1Value,0);
+    asignarLed2(pot1Value,0,0);
+  }else if(pot1Value <= 1000){
+    asignarLed1(pot1Value,pot1Value,pot1Value);
+    asignarLed2(0,0,0);
   }
 }
 
-void auxiliar(){
-  int pot1Value = analogRead(p1);
-  if(pot1Value>=20 && pot1Value<50){
-       asignarLed1(0,0,50);
-  }else if(pot1Value>=50 && pot1Value<100){
-       asignarLed1(0,0,100);
-  }else if(pot1Value>=100 && pot1Value<150){
-       asignarLed1(0,0,150);
-  }else if(pot1Value>=150 && pot1Value<200){
-       asignarLed1(0,0,200);
-  }else if(pot1Value>=200 && pot1Value<250){
-       asignarLed1(0,0,255);
-  }else if(pot1Value>=250 && pot1Value<300){
-       asignarLed1(0,50,50);
-  }else if(pot1Value>=300 && pot1Value<350){
-       asignarLed1(0,100,100);
-  }else if(pot1Value>=350 && pot1Value<400){
-       asignarLed1(0,150,150);
-  }else if(pot1Value>=400 && pot1Value<450){
-       asignarLed1(0,200,200);
-  }else if(pot1Value>=450 && pot1Value<500){
-       asignarLed1(0,255,255);
-  }else if(pot1Value>=500 && pot1Value<550){
-       asignarLed1(50,50,50);
-  }else if(pot1Value>=550 && pot1Value<600){
-       asignarLed1(100,100,100);
-  }else if(pot1Value>=600 && pot1Value<650){
-       asignarLed1(150,150,150);
-  }else if(pot1Value>=650 && pot1Value<700){
-       asignarLed1(200,200,200);
-  }else if(pot1Value>=700 && pot1Value<750){
-       asignarLed1(255,255,255);
-  }
-}
 
 void asignarLed1(int r, int g, int b){
   digitalWrite(green1, g);
@@ -90,8 +75,12 @@ void asignarLed2(int r, int g, int b){
   digitalWrite(red2, r);
 }
 
-void hacerSonarBuzzer(){
-    tone(pinbuzzer, valorBuzzer);
+void hacerSonarBuzzer(int frecuencia){
+    if(frecuencia <= 10){
+      mutearBuzzer();
+    }else{
+      tone(pinbuzzer, frecuencia);
+    }
 }
 
 void mutearBuzzer(){

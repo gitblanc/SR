3fe47ef00aceef833ef013624e1855120bbe1499
Eduardo Blanco Bielsa <87705461+gitblanc@users.noreply.github.com>
2022-12-01 09:11:11 +0100

    Update connect.php

--- Prácticas/P11/practica11-3/connect.php
+++ Prácticas/P11/practica11-3/connect.php
@@ -1,31 +1,31 @@
+<html>
+<body>
+
 <?php
 
-if(isset($_GET["temperatura"]) && isset($_GET["humedad"]) ) {
-   $temperature = $_GET["temperatura"]; // peticion GET para el valor de la temperatura
-   $humedad = $_GET["humedad"]; // peticion GET para el valor de la humedad
+$dbname = 'servidor_iot_robots';
+$dbuser = 'root';  
+$dbpass = ''; 
+$dbhost = 'localhost'; 
 
-   $servername = "localhost";
-   $username = "esp32";
-   $password = "1234";
-   $database_name = "servidor_iot_robots";
+$connect = @mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 
-   // Creamos la conexión MYSQL de PHP a MySQL server
-   $connection = new mysqli($servername, $username, $password, $database_name);
-   // Comprobamos la conexión
-   if ($connection->connect_error) {
-      die("MySQL connection failed: " . $connection->connect_error);
-   }
-
-   $sql = "INSERT INTO datos_sensor (temperatura,humedad) VALUES ($temperatura,$humedad)";
-
-   if ($connection->query($sql) === TRUE) {
-      echo "Datos insertados correctamente";
-   } else {
-      echo "Error: " . $sql . " => " . $connection->error;
-   }
-
-   $connection->close();
-} else {
-   echo "Temperatura o humedad incorrectas";
+if(!$connect){
+	echo "Error: " . mysqli_connect_error();
+	exit();
 }
+
+echo "Connection Success!<br><br>";
+
+$temperatura = $_GET["temperatura"];
+$humedad = $_GET["humedad"]; 
+
+
+$query = "INSERT INTO datos_sensor (temperatura, humedad) VALUES ('$temperatura', '$humedad')";
+$result = mysqli_query($connect,$query);
+
+echo "Insertion Success!<br>";
+
 ?>
+</body>
+</html>
